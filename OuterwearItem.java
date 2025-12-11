853dc685d9984ff2cd80c71260523560635243b6
tuTech <149540239+TulayTech@users.noreply.github.com>
2025-12-11 14:14:42 -0500

    Create OuterwearItem.java
    Represents an outerwear clothing item (jackets, coats, hoodies, etc..). Category is fixed to outerwear

---
+++ OuterwearItem.java
@@ -0,0 +1,15 @@
+/** 
+**Represents an outerwear clothing item (jackets, coats, hoodies, etc..)
+Category is fixed to outerwear
+*/
+
+public class OuterwearItem extends ClothingItem {
+	public OuterwearItem(String itemName,
+						String type,
+						String description,
+						String color,
+						String code) {
+		// Passes category "Outerwear" to the parent constructor
+		super(itemName, "Outerwear", type, description, color, code);
+	}
+}
